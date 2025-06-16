ad9178e3c6103cc68c46342772501f5d4cbdbb6a
ninelkaaaa <ninelsafarova55@gmail.com>
2025-06-16 20:17:59 +0500

    Обновление админ-панели

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ComplaintForm from './components/ComplaintForm';
 import ComplaintList from './components/ComplaintList';
 import './components/ComplaintForm.css';  // в ComplaintForm.js
-import './components/ComplaintList.css';  // в ComplaintList.js
+ // в ComplaintList.js
 
 const App = () => (
   <>
     <ComplaintForm />
-    <hr />
-    <ComplaintList />
   </>
 );
 
